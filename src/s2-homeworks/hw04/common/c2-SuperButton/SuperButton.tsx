968c6b0dc2e390e6d2d20ee73d08cf8bdd0d1dd1
dyv21 <denis@artkerr.com>
2024-07-01 20:33:54 +0300

    refatorimg

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,13 +17,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
+    const finalClassName = s.button + ' ' + s.default
       + (disabled ? ' ' + s.disabled : '')
       + (xType === 'red' ? ' ' + s.red : '' )
       + (xType === 'secondary' ? ' ' + s.secondary : '' )
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+
 
     return (
         <button
