2b78f857b1e5343947df5b353d53a9e0f8baa789
dyv21 <denis@artkerr.com>
2024-11-22 18:21:15 +0300

    hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,30 @@
+import {Dispatch} from "redux"
+
 const initState = {
-    isLoading: false,
+  isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
+export const loadingReducer = (state = initState, action: LoadingActionType): any => { // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case 'CHANGE_LOADING': {
+      return {...state, isLoading: !state.isLoading}
     }
+
+    default:
+      return state
+  }
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+  type: 'CHANGE_LOADING'
+  isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+  type: 'CHANGE_LOADING',
+  isLoading,
 })
+
+
+
